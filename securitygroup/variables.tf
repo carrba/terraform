09ef2e7f438166c06c5876c8ed7ce6f202005bf7
Brian Carr <b.carr@btinternet.com>
2021-04-11 13:23:05 +0100

    Updated securtygroup rules to be dynamic block

--- securitygroup/variables.tf
+++ securitygroup/variables.tf
@@ -5,3 +5,17 @@ variable "location" {
   type    = string
   default = "uksouth"
 }
+variable "nsg_rule" {
+  description = "nsg rules"
+  type = list(object({
+    name = string
+    priority = string
+    direction = string
+    access = string
+    protocol = string
+    source_port_range = string
+    destination_port_range = string
+    source_address_prefix = string
+    destination_address_prefix = string
+  }))
+}
